4b821d03778a42a5681462056cc50f36374dee44
gagax1234 <44903027+gagax1234@users.noreply.github.com>
2020-10-03 15:29:10 +0200

    buffer undo states to disk to save memory

--- src/UndoSystem.h
+++ src/UndoSystem.h
@@ -10,12 +10,34 @@
 // in a previous iteration
 // this looked a lot more sophisticated
 // while this is not memory efficient this turns out to be incredibly robust and flexible
-struct ScriptState {
+class ScriptState {
+private:
+	struct ScriptStateHeader {
+		int32_t message_len;
+		int32_t actions_len;
+		int32_t selection_len;
+		int32_t raw_len;
+	};
+
+	Funscript::FunscriptData data;
+public:
+	inline ScriptStateHeader Header() {
+		ScriptStateHeader header;
+		header.actions_len = data.Actions.size();
+		header.message_len = Message.size();
+		header.selection_len = data.selection.size();
+		header.raw_len = data.RawActions.size();
+		return header;
+	}
+	inline bool IsOnDisk() const { return DiskPointer != -1; }
+	void WriteToDisk(int32_t diskPointer);
+	Funscript::FunscriptData& Data();
+
+	int32_t DiskPointer = -1;
 	std::string Message;
-	Funscript::FunscriptData Data;
 
 	ScriptState(const std::string& msg, const Funscript::FunscriptData& data)
-		: Message(msg), Data(data) {}
+		: Message(msg), data(data), DiskPointer(-1) {}
 };
 
 class UndoSystem
@@ -26,6 +48,8 @@ public:
 	// because std::stack can't be iterated
 	std::vector<ScriptState> UndoStack;
 	std::vector<ScriptState> RedoStack;
+	int32_t SystemDiskPointer = 0;
+	const int32_t MaxScriptStateInMemory = 100;
 
 	void Snapshot(const std::string& msg, bool clearRedo = true) noexcept;
 	void Undo() noexcept;
