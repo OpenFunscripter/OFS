6b73e9f7c9bdab61ff3cd0d2c1ade0afcbcfd106
gagax1234 <44903027+gagax1234@users.noreply.github.com>
2020-11-17 16:26:35 +0100

    orthographic projection

--- src/gl/Simulator3D.cpp
+++ src/gl/Simulator3D.cpp
@@ -5,6 +5,8 @@
 #include "imgui.h"
 #include "ImGuizmo.h"
 
+#include "imGuIZMOquat.h"
+
 // cube pos + normals
 constexpr float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -51,17 +53,22 @@ constexpr float vertices[] = {
 };
 
 
-constexpr float length = 3.f;
-constexpr float distance = 3.f;
-constexpr float cubeSize = 0.75f;
+constexpr float simLength = 2.f;
+constexpr float simDistance = 5.f;
+constexpr float simCubeSize = 0.5f;
 
 void Simulator3D::reset() noexcept
 {
     view = glm::mat4(1.f);
-    viewPos = glm::vec3(0.f);
+    viewPos = glm::vec3(0.f, 0.f, 0.f);
+    view = glm::translate(view, viewPos);
 
     translation = glm::mat4(1.f);
-    translation = glm::translate(translation, glm::vec3(0.f, 0.f, -distance));
+    translation = glm::translate(translation, glm::vec3(0.f, 0.f, -simDistance));
+
+    lightPos = glm::vec3(0.f, 0.f, 0.f);
+
+    Zoom = 3.f;
 }
 
 void Simulator3D::setup() noexcept
@@ -102,23 +109,21 @@ void Simulator3D::ShowWindow(bool* open) noexcept
     }
     if (rollIndex < loadedScriptsCount) {
         roll = app->LoadedFunscripts[rollIndex]->GetPositionAtTime(currentMs) - 50.f;
-        roll = 30.f * (roll / 50.f);
+        roll = (rollRange/2.f) * (roll / 50.f);
     }
     if (pitchIndex < loadedScriptsCount) {
         pitch = app->LoadedFunscripts[pitchIndex]->GetPositionAtTime(currentMs) - 50.f;
-        pitch = 45.f * (pitch / 50.f);
+        pitch = (pitchRange/2.f) * (pitch / 50.f);
     }
     if (twistIndex < loadedScriptsCount) {
         yaw = app->LoadedFunscripts[twistIndex]->GetPositionAtTime(currentMs) - 50.f;
-        yaw = 90.f * (yaw / 50.f);
+        yaw = (twistRange/2.f) * (yaw / 50.f);
     }
 
-    constexpr float length = 3.f;
-    constexpr float distance = 3.f;
-    constexpr float cubeSize = 0.75f;
-
     auto viewport = ImGui::GetMainViewport();
-    projection = glm::perspective(glm::radians(90.f), viewport->Size.x / viewport->Size.y, 0.1f, 100.0f); 
+    
+    float ratio = viewport->Size.x / viewport->Size.y;
+    projection = glm::ortho(-Zoom*ratio, Zoom*ratio, -Zoom, Zoom, 0.1f, 100.f);
     
     ImGui::Begin("Simulator 3D", open, ImGuiWindowFlags_None);
 
@@ -126,14 +131,12 @@ void Simulator3D::ShowWindow(bool* open) noexcept
         reset();
     }
 
-    ImGui::ColorEdit4("Box", &boxColor.Value.x);
-    ImGui::ColorEdit4("Container", &containerColor.Value.x);
 
+    ImGuizmo::SetOrthographic(true);
     ImGuizmo::SetDrawlist(ImGui::GetForegroundDrawList());
     ImGuizmo::SetRect(viewport->Pos.x, viewport->Pos.y, viewport->Size.x, viewport->Size.y);
 
     if (ImGui::Button("Move", ImVec2(-1.f, 0.f))) { TranslateEnabled = !TranslateEnabled; }
-    if (ImGui::Button("Gimbal", ImVec2(-1.f, 0.f))) { Gimbal = !Gimbal; }
     if (TranslateEnabled) {
         if (ImGuizmo::Manipulate(glm::value_ptr(view),
             glm::value_ptr(projection),
@@ -148,6 +151,10 @@ void Simulator3D::ShowWindow(bool* open) noexcept
         }
     }
 
+    ImGui::SliderFloat("Distance", &Zoom, 0.1f, MaxZoom);
+    //ImGui::SliderFloat3("Light", glm::value_ptr(lightPos), -2.f, 2.f);
+
+
     // TODO: use more efficient way of doing getting this vector...
     glm::vec3 direction;
     {
@@ -157,27 +164,24 @@ void Simulator3D::ShowWindow(bool* open) noexcept
         directionMtx = glm::rotate(directionMtx, glm::radians(pitch), glm::vec3(1.f, 0.f, 0.f));
         direction = glm::vec3(directionMtx[1][0], directionMtx[1][1], directionMtx[1][2]);
         direction = glm::normalize(direction);
+    }
+    ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+    imguiGizmo::setDirectionColor(ImVec4(0.5, 1.0, 0.3, 1.0)); // change ArrowDirection color
+    ImGui::gizmo3D("Direction", -direction, ImGui::GetContentRegionAvail().x, imguiGizmo::modeDirection | imguiGizmo::modeDirPlane);
+    ImGui::PopItemFlag();
 
-        if (Gimbal) {
-            if (ImGuizmo::Manipulate(glm::value_ptr(view),
-                glm::value_ptr(projection),
-                ImGuizmo::OPERATION::ROTATE,
-                ImGuizmo::MODE::LOCAL,
-                glm::value_ptr(directionMtx))) {
-
-                auto g = ImGui::GetCurrentContext();
-                auto window = ImGui::GetCurrentWindow();
-                g->HoveredRootWindow = window;
-                g->HoveredWindow = window;
-                g->HoveredDockNode = window->DockNode;
-            }
-        }
+    if (ImGui::CollapsingHeader("Settings")) {
+        ImGui::ColorEdit4("Box", &boxColor.Value.x);
+        ImGui::ColorEdit4("Container", &containerColor.Value.x);
+        ImGui::InputFloat("Roll deg", &rollRange);
+        ImGui::InputFloat("Pitch deg", &pitchRange);
+        ImGui::InputFloat("Twist deg", &twistRange);
     }
 
     auto ScriptCombo = [](auto Id, int32_t* index) {
         auto app = OpenFunscripter::ptr;
         int32_t loadedScriptCount = app->LoadedFunscripts.size();
-        if (ImGui::BeginCombo(Id, *index < loadedScriptCount ? app->LoadedFunscripts[*index]->metadata.title.c_str() : "none")) {
+        if (ImGui::BeginCombo(Id, *index < loadedScriptCount ? app->LoadedFunscripts[*index]->metadata.title.c_str() : "none", ImGuiComboFlags_PopupAlignLeft)) {
             for (int i = 0; i < app->LoadedFunscripts.size(); i++) {
                 auto&& script = app->LoadedFunscripts[i];
                 if (ImGui::Selectable(script->metadata.title.c_str()) || ImGui::IsItemHovered()) {
@@ -202,7 +206,7 @@ void Simulator3D::ShowWindow(bool* open) noexcept
     
     constexpr float antiZBufferFight = 0.005f;
 
-    const float cubeHeight = (length + cubeSize) * ((scriptPos) / 100.f);
+    const float cubeHeight = (simLength + simCubeSize) * ((scriptPos) / 100.f);
     // container model matrix
     containerModel = glm::mat4(1.f);
     containerModel = glm::translate(containerModel, ((direction * ((cubeHeight + antiZBufferFight)/2.f))) + glm::vec3(matrixTranslation[0], matrixTranslation[1], matrixTranslation[2]));
@@ -211,28 +215,26 @@ void Simulator3D::ShowWindow(bool* open) noexcept
     containerModel = glm::rotate(containerModel, glm::radians(pitch), glm::vec3(1.f, 0.f, 0.f));
     
 
-    containerModel = glm::scale(containerModel, glm::vec3(cubeSize, (length+cubeSize) - cubeHeight, cubeSize)*1.01f);
+    containerModel = glm::scale(containerModel, glm::vec3(simCubeSize, (simLength + simCubeSize) - cubeHeight, simCubeSize)*1.01f);
 
     // box model matrix
     boxModel = glm::mat4(1.f);
-    boxModel = glm::translate(boxModel, (-(direction * ((length + cubeSize) - (cubeHeight - antiZBufferFight)))/2.f) + glm::vec3(matrixTranslation[0], matrixTranslation[1], matrixTranslation[2]));
+    boxModel = glm::translate(boxModel, (-(direction * ((simLength + simCubeSize) - (cubeHeight - antiZBufferFight)))/2.f) + glm::vec3(matrixTranslation[0], matrixTranslation[1], matrixTranslation[2]));
     boxModel = glm::rotate(boxModel, glm::radians(roll), glm::vec3(0.f, 0.f, 1.f));
     boxModel = glm::rotate(boxModel, glm::radians(yaw), glm::vec3(0.f, 1.f, 0.f));
     boxModel = glm::rotate(boxModel, glm::radians(pitch), glm::vec3(1.f, 0.f, 0.f));
-    boxModel = glm::scale(boxModel, glm::vec3(cubeSize, cubeHeight, cubeSize));
+    boxModel = glm::scale(boxModel, glm::vec3(simCubeSize, cubeHeight, simCubeSize));
 }
 
 void Simulator3D::render() noexcept
 {
     if (!OpenFunscripter::ptr->settings->data().show_simulator_3d) return;
-    constexpr float lightPos[3] {0.f, 0.f, -1.f };
     
     glClear(GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
 
-
     lightShader->use();
-    lightShader->LightPos(&lightPos[0]);
+    lightShader->LightPos(glm::value_ptr(lightPos));
     lightShader->ProjectionMtx(glm::value_ptr(projection));
     lightShader->ViewMtx(glm::value_ptr(view));
     lightShader->ViewPos(glm::value_ptr(viewPos));
--- src/gl/Simulator3D.h
+++ src/gl/Simulator3D.h
@@ -18,7 +18,14 @@ private:
 	unsigned int VBO = 0;
 	unsigned int cubeVAO = 0;
 	bool TranslateEnabled = false;
-	bool Gimbal = false;
+
+	static constexpr float MaxZoom = 10.f;
+	float Zoom = 3.f;
+
+
+	float rollRange = 60.f;
+	float pitchRange = 90.f;
+	float twistRange = 180.f;
 
     glm::mat4 projection;
 	glm::mat4 view;
@@ -28,14 +35,15 @@ private:
 	glm::mat4 containerModel;
 
 	glm::vec3 viewPos;
+	glm::vec3 lightPos;
 
 	int32_t posIndex = 0;
 	int32_t rollIndex = 1;
 	int32_t pitchIndex = 2;
 	int32_t twistIndex = 3;
 	
-	ImColor boxColor = IM_COL32(245, 164, 66, (int)(0.75f * 255));
-	ImColor containerColor = IM_COL32(66, 135, 245, (int)(0.4f * 255));
+	ImColor boxColor = IM_COL32(245, 164, 66, (int)(0.8f * 255));
+	ImColor containerColor = IM_COL32(66, 135, 245, (int)(0.6f * 255));
 
 	void reset() noexcept;
 public:
