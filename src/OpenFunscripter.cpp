36bbdbc26c1ea9dc03c9409b9e3d68ff40ca814a
gagax1234 <44903027+gagax1234@users.noreply.github.com>
2020-12-18 13:20:37 +0100

    heatmap gen

--- src/OpenFunscripter.cpp
+++ src/OpenFunscripter.cpp
@@ -2791,6 +2791,8 @@ void OpenFunscripter::UpdateTimelineGradient(ImGradient& grad)
         IM_COL32(0xFF, 0xFF, 0x00, 0xFF),
         IM_COL32(0xFF, 0x00, 0x00, 0xFF),
     };
+
+
     ImGradient HeatMap;
     float pos = 0.0f;
     for (auto& col : heatColor) {
@@ -2798,6 +2800,8 @@ void OpenFunscripter::UpdateTimelineGradient(ImGradient& grad)
         pos += (1.f / (heatColor.size() - 1));
     }
     HeatMap.refreshCache();
+
+
 
     auto getSegments = [](const std::vector<FunscriptAction>& actions, int32_t gapDurationMs) -> std::vector<std::vector<FunscriptAction>> {       
         int prev_direction = 0; // 0 neutral 0< up 0> down
@@ -2837,12 +2841,17 @@ void OpenFunscripter::UpdateTimelineGradient(ImGradient& grad)
         }
     };
 
+
     // this comes fairly close to what ScriptPlayer's heatmap looks like
     const float totalDurationMs = player.getDuration() * 1000.0;
-    const float kernel_size_ms = 5000.f;
-    const float max_actions_in_kernel = 24.5f;
+    constexpr float kernel_size_ms = 2500.f;
+    constexpr float max_actions_in_kernel = 24.5f / (5.f / (kernel_size_ms/1000.f));
 
     ImColor color(0.f, 0.f, 0.f, 1.f);
+    
+    constexpr int32_t max_samples = 3;
+    std::vector<float> samples;
+    samples.reserve(max_samples);
 
     auto segments = getSegments(ActiveFunscript()->Actions(), 10000);
     for (auto& segment : segments) {
@@ -2866,11 +2875,29 @@ void OpenFunscripter::UpdateTimelineGradient(ImGradient& grad)
             }
             kernel_offset += kernel_size_ms;
 
-
             float actionsRelToMax = Util::Clamp((float)actions_in_kernel / max_actions_in_kernel, 0.0f, 1.0f);
+            if (samples.size() == max_samples+1) {
+                samples.erase(samples.begin());
+            }
+            samples.emplace_back(actionsRelToMax);
+
+            auto getAverage = [](std::vector<float>& samples) {
+                float result = 0.f;
+                for (auto&& sample : samples) {
+                    result += sample;
+                }
+                result /= (float)samples.size();
+                return result;
+            };
+
+            if (samples.size() > 1) {
+                actionsRelToMax = getAverage(samples);
+            }
+
             HeatMap.getColorAt(actionsRelToMax, (float*)&color.Value);
             float markPos = kernel_offset  / totalDurationMs;
             grad.addMark(markPos, color);
+
         } while (kernel_offset < (segment.front().at + durationMs));
         grad.addMark((kernel_offset + 1.f) / totalDurationMs, IM_COL32(0, 0, 0, 255));
     }
