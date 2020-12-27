77d95bba25773475260b5faf2df634817c1e11af
gagax1234 <44903027+gagax1234@users.noreply.github.com>
2020-12-27 22:18:32 +0100

    fix 3d sim

--- src/OpenFunscripter.cpp
+++ src/OpenFunscripter.cpp
@@ -1212,7 +1212,6 @@ void OpenFunscripter::render() noexcept
 {
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
     // Update and Render additional Platform Windows
     // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
     //  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
