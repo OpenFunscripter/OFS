66e30ce0d73961ae9730b0ec4a5c2456b3921a8c
OpenFunscripter <86110971+OpenFunscripter@users.noreply.github.com>
2022-09-28 18:17:32 +0200

    on second thought switch to opengl 3.0 es

--- src/OpenFunscripter.cpp
+++ src/OpenFunscripter.cpp
@@ -165,7 +165,7 @@ bool OpenFunscripter::setup(int argc, char* argv[])
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
     // antialiasing
     // this caused problems in my linux testing
